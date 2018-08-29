edb697788925b6096e40d3f66f2f00d0e1929e92
XGG <zapperhatz26@gmail.com>
2018-08-29 20:44:40 +0800

    sql update

--- database/presshub.sql
+++ database/presshub.sql
@@ -1,79 +1,278 @@
-create database if not exists presshub;
-	use presshub;
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 29, 2018 at 02:44 PM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 5.6.36
 
-create table users(
-id int primary key not null auto_increment,
-username varchar(32) not null,
-password varchar(100) not null,
-first_name varchar(50) not null,
-middle_name varchar(50),
-last_name varchar(50) not null,
-designation varchar(4) not null,
-contact_no varchar(10) not null,
-email_addr varchar(30) not null,
-date_created datetime default CURRENT_TIMESTAMP(),
-date_updated datetime ON UPDATE CURRENT_TIMESTAMP(),
-is_admin varchar(1) not null,
-is_active varchar(1) not null
-);
-
-create table issue(
-id int primary key not null auto_increment,
-nickname varchar(50) not null,
-date_started datetime default  CURRENT_TIMESTAMP,
-date_last_updated datetime on update CURRENT_TIMESTAMP,
-deadline datetime not null,
-date_finished date,
-status varchar(3),
-is_archived varchar(1) default "N"
-);
-
-create table article(
-id int primary key not null auto_increment,
-date_created datetime default CURRENT_TIMESTAMP(),
-date_updated datetime ON UPDATE CURRENT_TIMESTAMP(),
-status varchar(3),
-title varchar(100) not null,
-body varchar(65536),
-deadline date,
-date_finished date
-);
-
-create table comment(
-id int primary key not null auto_increment,
-user int not null,
-comment varchar(65536) not null,
-date_created datetime default CURRENT_TIMESTAMP(),
-date_updated datetime ON UPDATE CURRENT_TIMESTAMP(),
-is_edited varchar(1) default "N",
-is_resolved varchar(1) default "N"
-);
-
-create table reply(
-id int primary key not null auto_increment,
-parent int not null,
-user int not null,
-body varchar(65536) not null,
-date_created datetime default CURRENT_TIMESTAMP()
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-create table user_issue(
-user int not null,
-issue int not null
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-create table user_article(
-user int not null,
-article int not null
-);
+--
+-- Database: `presshub`
+--
 
-create table designation(
-id varchar(4),
-description varchar(20)
-);
+-- --------------------------------------------------------
 
-create table status(
-id varchar(1) not null,
-description varchar(20) not null
-);
+--
+-- Table structure for table `announcement`
+--
+
+CREATE TABLE `announcement` (
+  `id` int(11) NOT NULL,
+  `body` mediumtext,
+  `date_created` datetime DEFAULT CURRENT_TIMESTAMP,
+  `author` int(11) NOT NULL,
+  `is_active` varchar(1) DEFAULT 'Y'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article`
+--
+
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `date_created` datetime DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `status` varchar(3) DEFAULT NULL,
+  `title` varchar(100) NOT NULL,
+  `body` mediumtext,
+  `deadline` date DEFAULT NULL,
+  `date_finished` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
+  `comment` mediumtext NOT NULL,
+  `date_created` datetime DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `is_edited` varchar(1) DEFAULT 'N',
+  `is_resolved` varchar(1) DEFAULT 'N'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `designation`
+--
+
+CREATE TABLE `designation` (
+  `id` varchar(4) DEFAULT NULL,
+  `description` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `designation`
+--
+
+INSERT INTO `designation` (`id`, `description`) VALUES
+('ADM', 'ADMINISTRATOR'),
+('EIC', 'Editor-in-Chief'),
+('PHO', 'PHOTOGRAPHER'),
+('FEA', 'FEATURE WRITER');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issue`
+--
+
+CREATE TABLE `issue` (
+  `id` int(11) NOT NULL,
+  `nickname` varchar(50) NOT NULL,
+  `date_started` datetime DEFAULT CURRENT_TIMESTAMP,
+  `date_last_updated` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `deadline` datetime NOT NULL,
+  `date_finished` date DEFAULT NULL,
+  `status` varchar(3) DEFAULT NULL,
+  `is_archived` varchar(1) DEFAULT 'N'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `issue`
+--
+
+INSERT INTO `issue` (`id`, `nickname`, `date_started`, `date_last_updated`, `deadline`, `date_finished`, `status`, `is_archived`) VALUES
+(4, 'Test Issue', '2018-08-09 00:00:00', NULL, '2018-08-31 00:00:00', NULL, 'O', 'N');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reply`
+--
+
+CREATE TABLE `reply` (
+  `id` int(11) NOT NULL,
+  `parent` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
+  `body` mediumtext NOT NULL,
+  `date_created` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `id` varchar(1) NOT NULL,
+  `description` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `designation` varchar(4) NOT NULL,
+  `contact_no` varchar(10) NOT NULL,
+  `email_addr` varchar(30) NOT NULL,
+  `date_created` datetime DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `is_admin` varchar(1) NOT NULL,
+  `is_active` varchar(1) NOT NULL DEFAULT 'N'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `middle_name`, `last_name`, `designation`, `contact_no`, `email_addr`, `date_created`, `date_updated`, `is_admin`, `is_active`) VALUES
+(5, 'rain', 'MjQzZGMxMzkzMGIyNTkxMWViOWMyMzJhNzllMzcwN2I=', 'Rain', 'Maristela', 'Pioquinto', 'ADM', '9177994321', 'rain@gmail.com', '2018-08-14 21:44:55', NULL, 'Y', 'N'),
+(6, 'josh', 'NjY5YWYwMTZhZDdkZjg2ZTU1N2ZlY2U0MWIwZmRjZjQ=', 'Josh', 'M', 'Kotlin', 'FEA', '9875456', 'qweqwe@dasd.cd', '2018-08-29 12:09:24', NULL, 'Y', 'N');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_article`
+--
+
+CREATE TABLE `user_article` (
+  `user` int(11) NOT NULL,
+  `article` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_issue`
+--
+
+CREATE TABLE `user_issue` (
+  `user` int(11) NOT NULL,
+  `issue` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `announcement`
+--
+ALTER TABLE `announcement`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `issue`
+--
+ALTER TABLE `issue`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reply`
+--
+ALTER TABLE `reply`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `announcement`
+--
+ALTER TABLE `announcement`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `article`
+--
+ALTER TABLE `article`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `issue`
+--
+ALTER TABLE `issue`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `reply`
+--
+ALTER TABLE `reply`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
