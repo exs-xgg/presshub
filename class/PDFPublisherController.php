ca06b0f938fa92d01936336fac4847a27e8360e5
Romeo David <zapperhatz26@gmail.com>
2019-02-02 09:37:32 +0800

    2-3 column dynamic

--- class/PDFPublisherController.php
+++ class/PDFPublisherController.php
@@ -6,7 +6,7 @@ class PDF extends FPDF
 {
 protected $col = 0; // Current column
 protected $y0;      // Ordinate of column start
-
+protected $how_many_columns = 3;
 function Header()
 {
     // Page header
@@ -37,31 +37,69 @@ function Footer()
 function SetCol($col)
 {
     // Set position at a given column
-    $this->col = $col;
-    $x = 10+$col*65;
-    $this->SetLeftMargin($x);
-    $this->SetX($x);
+//if column is 3
+	if ($this->how_many_columns==3) {
+		$this->col = $col;
+    	$x = 10+$col*65;
+	   	$this->SetLeftMargin($x);
+	   	$this->SetX($x);
+	}elseif ($this->how_many_columns==2) {
+		$this->col = $col;
+	    $x = 10+$col*96;
+	    $this->SetLeftMargin($x);
+	    $this->SetX($x);
+	}
+ 	
+
+//if column is 2
+    // $this->col = $col;
+    // $x = 10+$col*96;
+    // $this->SetLeftMargin($x);
+    // $this->SetX($x);
 }
 
 function AcceptPageBreak()
 {
     // Method accepting or not automatic page break
-    if($this->col<2)
-    {
-        // Go to next column
-        $this->SetCol($this->col+1);
-        // Set ordinate to top
-        $this->SetY($this->y0);
-        // Keep on page
-        return false;
+    if ($this->how_many_columns==3) {
+    	if($this->col<2)
+		    {
+		        // Go to next column
+		        $this->SetCol($this->col+1);
+		        // Set ordinate to top
+		        $this->SetY($this->y0);
+		        // Keep on page
+		        return false;
+		    }
+		    else
+		    {
+		        // Go back to first column
+		        $this->SetCol(0);
+		        // Page break
+		        return true;
+		    }
+    }elseif ($this->how_many_columns==2) {
+    	if($this->col<2)
+	    {
+	        // Go to next column
+	        $this->SetCol($this->col+1);
+	        // Set ordinate to top
+	        $this->SetY($this->y0);
+	        // Keep on page
+	        return false;
+	    }
+	    else
+	    {
+	        // Go back to first column
+	        $this->SetCol(0);
+	        // Page break
+	        return true;
+	    }
     }
-    else
-    {
-        // Go back to first column
-        $this->SetCol(0);
-        // Page break
-        return true;
-    }
+    
+
+
+    
 }
 
 function ChapterTitle($num, $label)
@@ -77,12 +115,23 @@ function ChapterTitle($num, $label)
 
 function ChapterBody($file)
 {
+	// echo $this->how_many_columns;
+	// $param1 = 60;
+ //    	$param2 = 5;
+	$this->SetCol(0);
     // Read text file
     $txt = file_get_contents($file);
     // Font
     $this->SetFont('Times','',12);
-    // Output text in a 6 cm width column
-    $this->MultiCell(100,10,'Across the other side of the world, the merchant and his ship had arrived at their destination.  The people were so pleased to see them and were so welcoming that the merchant decided to send some presents to their king and queen.  The king and queen were so delighted that they invited them all to a feast.  But, believe it or not, as soon as the food was brought in hundreds of rats appeared as if by magic and gobbled it all up before they had a chance to eat.
+if ($this->how_many_columns==3) {
+    	$param1 = 60;
+    	$param2 = 5;
+    }elseif ($this->how_many_columns==2) {
+    	$param1 = 95;
+    	$param2 = 5;
+    }
+//IF COLUMN IS 2 = $this->MultiCell(95,5,
+    $this->MultiCell($param1,$param2,'Across the other side of the world, the merchant and his ship had arrived at their destination.  The people were so pleased to see them and were so welcoming that the merchant decided to send some presents to their king and queen.  The king and queen were so delighted that they invited them all to a feast.  But, believe it or not, as soon as the food was brought in hundreds of rats appeared as if by magic and gobbled it all up before they had a chance to eat.
 
 “Oh dear” said the king “this is always happening – I never get a chance to eat my apple pie.  What can I do?”
 
@@ -96,18 +145,7 @@ When the ship returned to London Dick was overwhelmed with the amount of gold th
 
 “Oh dear” said the king “this is always happening – I never get a chance to eat my apple pie.  What can I do?”
 
-“I have an idea” said the merchant “I have a very special cat which has travelled with me all the way from London, and she will gobble up your rats faster than they gobbled up your feast.”
-
-Sure enough, to the king and queen’s joy, the next time a feast was prepared and the rats appeared, the cat pounced and killed all the rats as quick as lightening.
-
-The king and queen danced for joy and gave the merchant a ship full of gold in return for the very special cat.
-
-When the ship returned to London Dick was overwhelmed with the amount of gold the merchant gave him for his cat.  Over the years he used his money so wisely, and did so much good for all the people around him and who worked for him, that he was elected Lord Mayor of the City of London three times.  But he never forgot his kind friend the merchant, who had been so honest in giving him all the money that the cat had earned and kept nothing for himself. When Dick grew up he fell in love with Alice, the merchant’s beautiful daughter, and married her.  They lived happily ever after as people do in stories. Across the other side of the world, the merchant and his ship had arrived at their destination.  The people were so pleased to see them and were so welcoming that the merchant decided to send some presents to their king and queen.  The king and queen were so delighted that they invited them all to a feast.  But, believe it or not, as soon as the food was brought in hundreds of rats appeared as if by magic and gobbled it all up before they had a chance to eat.
-
-“Oh dear” said the king “this is always happening – I never get a chance to eat my apple pie.  What can I do?”
-
-“I have an idea” said the merchant “I have a very special cat which has travelled with me all the way from London, and she will gobble up your rats faster than they gobbled up your feast.”
-
+“I have an idea” said the merchant “I have a very special cat which has travelled with me all the way from London, and she will gobble up your rats faster than they gobbled up 
 Sure enough, to the king and queen’s joy, the next time a feast was prepared and the rats appeared, the cat pounced and killed all the rats as quick as lightening.
 
 The king and queen danced for joy and gave the merchant a ship full of gold in return for the very special cat.
@@ -121,12 +159,14 @@ When the ship returned to London Dick was overwhelmed with the amount of gold th
     $this->SetCol(0);
 }
 
-function PrintChapter($num, $title, $file)
+function PrintChapter($num, $title, $file,$how_many_columns)
 {
     // Add chapter
+    $this->how_many_columns = $how_many_columns;
     $this->AddPage();
     $this->ChapterTitle($num,$title);
     $this->ChapterBody($file);
+    // echo "$how_many_columns";
 }
 }
 
@@ -134,6 +174,6 @@ $pdf = new PDF();
 $title = '20000 Leagues Under the Seas';
 $pdf->SetTitle($title);
 $pdf->SetAuthor('Jules Verne');
-$pdf->PrintChapter(1,'A RUNAWAY REEF','http://localhost/js/10.txt');
-$pdf->PrintChapter(2,'THE PROS AND CONS','http://localhost/js/20.txt');
+$pdf->PrintChapter(1,'A RUNAWAY REEF','http://localhost/js/10.txt',3);
+$pdf->PrintChapter(2,'THE PROS AND CONS','http://localhost/js/20.txt',2);
 $pdf->Output();
